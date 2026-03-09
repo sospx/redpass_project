026cba68025e5195d0c4bd50dadbe20af2744435
Артем Данилов <lormy@MacBook-Air-Artem.local>
2026-03-10 02:42:12 +0300

    Настройка Alembic для асинхронных миграций

---
+++ db/database.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL, echo=False)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
---
+++ db/models.py
@@ -0,0 +1,29 @@
+import datetime
+from sqlalchemy import String, Integer, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
+
+    history = relationship("CheckHistory", back_populates="user")
+
+
+class CheckHistory(Base):
+    __tablename__ = "check_history"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True) # Может быть анонимным
+    masked_password: Mapped[str] = mapped_column(String) # Например: P******3
+    score: Mapped[int] = mapped_column(Integer) # Оценка zxcvbn (0-4)
+    is_leaked: Mapped[bool] = mapped_column(Boolean, default=False)
+    leak_count: Mapped[int] = mapped_column(Integer, default=0)
+    checked_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
+
+    user = relationship("User", back_populates="history")
