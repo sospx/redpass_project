8d107d7dabf5845b1fcde445391663dc58b321b9
sospx <avatarcum@mail.ru>
2026-03-10 13:02:11 +0300

    Испаравлена ошибка импорта в models.py

--- db/models.py
+++ db/models.py
@@ -1,7 +1,7 @@
 import datetime
 from sqlalchemy import String, Integer, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from database import Base
+from db.database import Base
 
 class User(Base):
     __tablename__ = "users"
