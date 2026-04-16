baa7a48c06b3409dd99fdf25e1ae2909cfa29aad
d3e3df <artemgerasimov646@gmail.com>
2026-04-17 00:28:02 +0300

    feat: add password generator page

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from routers import auth, password
-
 
 app = FastAPI(
     title="Redpass",
     description="Сервис проверки стойкости паролей и их наличие в базах утечек",
     version="1.0.0"
 )
+
 app.include_router(auth.router)
 app.include_router(password.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
 async def root():
     return {"message": "API is running. Welcome to Redpass!"}
+
+
+@app.get("/generator", response_class=HTMLResponse)
+async def generator_page():
+    with open("static/generator.html", "r", encoding="utf-8") as f:
+        return HTMLResponse(content=f.read())
