148f0f66b866597406bb0a6fc2af7b1437cfed36
lormy <Lormy7@yandex.ru>
2026-04-17 03:03:04 +0300

    Исправлен баг с падением приложения от ввода слишком длинного пароля

--- schemas/password.py
+++ schemas/password.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 
 
 class PasswordCheckRequest(BaseModel):
-    password: str
+    password: str = Field(..., max_length=72)
 
 
 class PasswordCheckResponse(BaseModel):
