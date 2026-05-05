c3770808b4734cb00202e7ec53c5bc16eaa5911b
sospx <avatarcum@mail.ru>
2026-05-06 01:47:58 +0300

    инициализатор лимитера запросов

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from starlette.responses import FileResponse
 from routers import auth, password
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+# Инициализация лимитера, определяющего клиента по IP
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(
     title="Redpass",
     description="Сервис проверки стойкости паролей и их наличие в базах утечек",
     version="1.0.0"
 )
+
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.include_router(auth.router)
 app.include_router(password.router)
